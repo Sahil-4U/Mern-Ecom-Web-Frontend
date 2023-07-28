2267a4ee6fbfbbec751f6a5f78b30d91cb00e5e4
Sahil <ss6611786@gmail.com>
2023-07-28 21:27:58 +0530

    i add some styling in register page

--- src/auth/Registerpage.js
+++ src/auth/Registerpage.js
@@ -38,38 +38,40 @@ function Registerpage() {
     }
     return (
         <Layout title={"Register-Ecom-app"}>
-            <h1 className="display-5 text-center py-2 text-info ">Register User</h1>
+            <div className='registerLayout'>
+                <h1 className="display-5 text-center py-2 text-info ">Register User</h1>
 
-            <form className='d-flex justify-content-center align-items-center px-5' onSubmit={handleSubmit}>
-                <div className='row'>
-                    <div className="mb-3 col-6">
-                        <label htmlFor="InputName" className="form-label">Name</label>
-                        <input type="name" className="form-control" id="InputName" placeholder='name' value={name} onChange={(e) => setName(e.target.value)} required />
-                    </div>
-                    <div className="mb-3 col-6">
-                        <label htmlFor="exampleInputEmail1" className="form-label">Email</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)} required />
+                <form className='d-flex justify-content-center align-items-center px-5' onSubmit={handleSubmit}>
+                    <div className='row'>
+                        <div className="mb-3 col-6">
+                            <label htmlFor="InputName" className="form-label">Name</label>
+                            <input type="name" className="form-control" id="InputName" placeholder='name' value={name} onChange={(e) => setName(e.target.value)} required />
+                        </div>
+                        <div className="mb-3 col-6">
+                            <label htmlFor="exampleInputEmail1" className="form-label">Email</label>
+                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)} required />
+
+                        </div>
+                        <div className="mb-3 col-6">
+                            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                            <input type="password" className="form-control" id="exampleInputPassword1" placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)} required />
+                        </div>
+                        <div className="mb-3 col-6">
+                            <label htmlFor="phone" className="form-label">Phone NO</label>
+                            <input type="text" className="form-control" data-mdb-input-mask="+91 999-999-999" id="phone" placeholder='phone no..' value={phone} onChange={(e) => setPhone(e.target.value)} required />
+                        </div>
+                        <div className="mb-3 col-6">
+                            <label htmlFor="address" className="form-label">Address</label>
+                            <input type="text" className="form-control" id="address" placeholder='address..' value={add} onChange={(e) => setAdd(e.target.value)} required />
+                        </div>
+                        <div className='text-center '>
+                            <button type="submit" className="btn btn-outline-primary  submit-button">Submit</button>
+                        </div>
 
                     </div>
-                    <div className="mb-3 col-6">
-                        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)} required />
-                    </div>
-                    <div className="mb-3 col-6">
-                        <label htmlFor="phone" className="form-label">Phone NO</label>
-                        <input type="text" className="form-control" data-mdb-input-mask="+91 999-999-999" id="phone" placeholder='phone no..' value={phone} onChange={(e) => setPhone(e.target.value)} required />
-                    </div>
-                    <div className="mb-3 col-6">
-                        <label htmlFor="address" className="form-label">Address</label>
-                        <input type="text" className="form-control" id="address" placeholder='address..' value={add} onChange={(e) => setAdd(e.target.value)} required />
-                    </div>
-                    <div className='text-center '>
-                        <button type="submit" className="btn btn-outline-primary  submit-button">Submit</button>
-                    </div>
 
-                </div>
-
-            </form>
+                </form>
+            </div>
 
         </Layout>
     )
