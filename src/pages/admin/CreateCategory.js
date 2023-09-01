c9ae93ba2b5ff8863fc0856c08f603e2884f6072
Sahil <ss6611786@gmail.com>
2023-09-01 23:54:37 +0530

    i have complete my data fetching process with the help of axios package

--- src/pages/admin/CreateCategory.js
+++ src/pages/admin/CreateCategory.js
@@ -1,9 +1,28 @@
 import React, { useState, useEffect } from 'react'
 import Layout from '../../components/Layout'
-import AdminMenu from '../../components/layout/AdminMenu'
+import AdminMenu from '../../components/layout/AdminMenu';
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
 function CreateCategory() {
     const [categories, setCategories] = useState([]);
+
+    const getallCat = async () => {
+        try {
+            const { data } = await axios.get('http://localhost:9090/category/all-category');
+            if (data.success) {
+                setCategories(data);
+                console.log(data);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error('Something went wrong in get all categories');
+        }
+
+    }
+    useEffect(() => {
+        getallCat();
+    }, [])
     return (
         <Layout title={'Dashboard-Create Category'}>
             <div className='container-fluid m-3 p-3'>
