c394c91c5f91c06fa75353eb9f0df6820a0200a4
Sahil <ss6611786@gmail.com>
2023-09-05 22:01:42 +0530

    update with of input component

--- src/components/Form/CategoryForm.js
+++ src/components/Form/CategoryForm.js
@@ -4,7 +4,7 @@ function CategoryForm({ handleSubmit, value, setValue }) {
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <div className='m-1 w-75'>
+                <div className='m-1 w-50'>
                     <input type='text' className='form-control ' placeholder='enter new category' value={value} onChange={(e) => setValue(e.target.value)} />
                 </div>
                 <button className='btn btn-outline-success my-1'>
